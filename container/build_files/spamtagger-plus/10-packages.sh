3efefec308f2697c509f7c2f2e24e5470c0e2dd5
John Mertz <git@john.me.tz>
2025-09-01 13:02:32 -0600

    Try getting all PHP dependencies using Composer during installation

--- container/build_files/spamtagger-plus/10-packages.sh
+++ container/build_files/spamtagger-plus/10-packages.sh
@@ -4,6 +4,7 @@ set ${CI:+-x} -euo pipefail
 # Unique SpamTagger Plus packages
 # */
 dnf -y install --setopt=install_weak_deps=False \
+  composer \
   httpd \
   mod_ssl \
   mariadb \
@@ -13,20 +14,24 @@ dnf -y install --setopt=install_weak_deps=False \
   net-snmp-devel \
   perl-DBD-MariaDB \
   perl-Net-SNMP \
-  php-bcmath \
-  php-common \
-  php-dba \
-  php-gd \
-  php-gmp \
-  php-intl \
-  php-mbstring \
-  php-pecl-memcache \
-  php-mysqlnd \
-  php-pear \
-  php-snmp \
-  php-soap \
-  php-xml \
-  php-pecl-zip
+  php
+# /*
+# Try getting all PHP dependencies through composer
+# php-bcmath \
+# php-common \
+# php-dba \
+# php-gd \
+# php-gmp \
+# php-intl \
+# php-mbstring \
+# php-pecl-memcache \
+# php-mysqlnd \
+# php-pear \
+# php-snmp \
+# php-soap \
+# php-xml \
+# php-pecl-zip
+# */
 
 # /* Missing, perhaps resolved my net-snmp
 # snmp-mibs-downloader
